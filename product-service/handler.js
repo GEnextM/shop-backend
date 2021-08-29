73677f0f3772b5bfa02bb063e2d698a55f5c3400
genextm <mrkrik2000@gmail.com>
2021-08-30 00:46:17 +0300

    feat: ready to PR

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,22 +1,74 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  var productslist = [{
+var productslist = [
+  {
     "count": 4,
     "description": "Short Product Description1",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+    "id": "1",
     "price": 2.4,
     "img": "https://vberloge.by/upload/iblock/fda/fda55401c6091b76da7e16e7a386dce1.jpg",
     "title": "Кот 'Барсик'"
-    },
-    {
-      "count": 6,
-      "description": "Short Product Description3",
-      "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
-      "price": 10,
-      "img": "https://www.aleshka.by/upload/iblock/527/ikkvi013%20Pink%20Bunny%201.jpg",
-      "title": "Вязаная игрушка Кролик"
-    }]
+  },
+  {
+    "count": 6,
+    "description": "Short Product Description3",
+    "id": "2",
+    "price": 10,
+    "img": "https://www.aleshka.by/upload/iblock/527/ikkvi013%20Pink%20Bunny%201.jpg",
+    "title": "Вязаная игрушка Кролик"
+  },
+  {
+    "count": 7,
+    "description": "Short Product Description2",
+    "id": "3",
+    "price": 23,
+    "img": "https://www.aleshka.by/upload/iblock/c98/9eozcdfezm65zd3u1y0lc5hqj2ey8s8f/0.jpg",
+    "title": "Интерактивная игрушка Star Wars Малыш Мандалорец"
+  },
+  {
+    "count": 12,
+    "description": "Short Product Description7",
+    "id": "4",
+    "price": 15,
+    "img": "https://www.aleshka.by/upload/iblock/174/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1.jpg",
+    "title": "Игрушка мягкая музыкальная Говорящий Щенок"
+  },
+  {
+    "count": 7,
+    "description": "Short Product Description2",
+    "id": "5",
+    "price": 23,
+    "img": "https://www.aleshka.by/upload/iblock/985/0.jpg",
+    "title": "Игрушка мягкая Медвежонок"
+  },
+  {
+    "count": 8,
+    "description": "Short Product Description4",
+    "id": "6",
+    "price": 15,
+    "img": "https://www.aleshka.by/upload/iblock/dd7/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1-%D0%B2%D0%BE%D1%81%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BB%D0%B5%D0%BD%D0%BE.jpg",
+    "title": "Игрушка мягконабивная Русалка Anastasia"
+  },
+  {
+    "count": 2,
+    "description": "Short Product Descriptio1",
+    "id": "7",
+    "price": 23,
+    "img": "https://www.aleshka.by/upload/iblock/e3f/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1.jpg",
+    "title": "Игрушка мягконабивная Принцесса Anna"
+  },
+  {
+    "count": 3,
+    "description": "Short Product Description7",
+    "id": "8",
+    "price": 15,
+    "img": "https://www.aleshka.by/upload/iblock/797/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-14.jpg",
+    "title": "Игрушка мягкая Петушок"
+  }
+]
+
+module.exports.hello = async (event) => {
+  
 
   return {
     statusCode: 200,
@@ -25,6 +77,25 @@ module.exports.hello = async (event) => {
       'Access-Control-Allow-Credentials': true,
     },
     body: JSON.stringify(productslist)
+          
+  };
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+
+module.exports.getproductbyid = async (event) => {
+
+  const { id } = event.queryStringParameters;
+  const found = productslist.find(product => product.id == id);
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(found)
+    // body: JSON.stringify(productslist[1])
           
   };
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
